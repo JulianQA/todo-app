d4a920c11198053a0bb7271658678a66d556663e
JulianQA <jfqa21julian@hotmail.com>
2022-11-02 18:15:37 -0500

    correcion de errores

--- src/Routes/Editpage/EditPage.jsx
+++ src/Routes/Editpage/EditPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ToDoForm } from '../../components/ToDoForm/ToDoForm';
 import { useMain } from '../../helpers/useMain';
 function EditPage() {
    const { toEditToDos, getTodo } = useMain();
+   const { slug } = useParams();
    const toDo = getTodo(Number(slug));
+   console.log(toDo)
    return (
       <ToDoForm
          textButton={'Editar'}
          textForm={'¡Edita tu TODO!'}
-         textToEdit={toDo}
+         textToEdit={toDo.text}
          submitEvent={(newText) => toEditToDos(Number(slug), newText)}
       />
    )
